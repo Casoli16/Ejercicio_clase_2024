27748b5861e2ec86b8a2bf63bdd93927f92ca4ba
Cubiasss-Eduardo <149213521+Cubiasss-Eduardo@users.noreply.github.com>
2024-02-13 09:51:56 -0600

    ya se puede runnear todo

--- dbEjercicio_clase.sql
+++ dbEjercicio_clase.sql
@@ -1,47 +1,7 @@
-DROP DATABASE Act1_DOC
+DROP DATABASE Act1_DOC;
 
-CREATE DATABASE Act1_DOC
-USE Act1_DOC
-
-	CREATE TABLE tb_mascotas(
-	id_mascota INT AUTO_INCREMENT PRIMARY KEY,
-	nombre_mascota VARCHAR(255),
-	edad_mascota VARCHAR(255),
-	peso_mascota FLOAT,
-	numero_duenio_mascota INT,
-	tamanio_mascota FLOAT,
-	id_genero INT,
-	CONSTRAINT fk_mascotas_razas FOREIGN KEY(id_genero) 
-	REFERENCES tb_generos(id_genero),
-	id_raza INT,
-	CONSTRAINT fk_mascotas_genero FOREIGN KEY(id_raza) 
-	REFERENCES tb_razas(id_raza),
-	id_tipo_animal INT,
-	CONSTRAINT fk_mascotas_tipos_animales FOREIGN KEY(id_tipo_animal) 
-	REFERENCES tb_tipos_animales(id_tipo_animal)
-);
-
-CREATE TABLE tb_citas(
-	id_cita INT AUTO_INCREMENT PRIMARY KEY,
-	causa_cita VARCHAR(255),
-	observaciones_cita VARCHAR(255),
-	medicasion_cita VARCHAR(255),
-	fecha_cita DATE,
-	id_mascota INT,
-	CONSTRAINT fk_cita_mascotas
-	FOREIGN KEY(id_mascota) 
-	REFERENCES tb_mascotas(id_mascota),
-	id_veterinario INT,
-	CONSTRAINT fk_cita_veterinarios
-	FOREIGN KEY(id_veterinario) 
-	REFERENCES tb_veterinarios(id_veterinario)
-);
-
-CREATE TABLE tb_generos(
-	id_genero INT AUTO_INCREMENT PRIMARY KEY,
-	nombre_genero VARCHAR(255)
-);
-
+CREATE DATABASE Act1_DOC;
+USE Act1_DOC;
 
 
 CREATE TABLE tb_especialidades_veterianos(
@@ -60,7 +20,10 @@ CREATE TABLE tb_razas(
 	nombre_raza VARCHAR(255)
 );
 
-INSERT INTO tb_razas(nombre_raza) VALUES ("Aguacatero");
+CREATE TABLE tb_generos(
+	id_genero INT AUTO_INCREMENT PRIMARY KEY,
+	nombre_genero VARCHAR(255)
+);
 
 CREATE TABLE tb_tipos_animales(
 	id_tipo_animal INT AUTO_INCREMENT PRIMARY KEY,
@@ -71,6 +34,24 @@ CREATE TABLE tb_veterinarias(
 	id_veterinaria INT AUTO_INCREMENT PRIMARY KEY,
 	nombre_veterinaria VARCHAR(255),
 	direccion_veterinaria VARCHAR(255)
+);
+
+CREATE TABLE tb_mascotas(
+	id_mascota INT AUTO_INCREMENT PRIMARY KEY,
+	nombre_mascota VARCHAR(255),
+	edad_mascota VARCHAR(255),
+	peso_mascota FLOAT,
+	numero_duenio_mascota INT,
+	tamanio_mascota FLOAT,
+	id_genero INT,
+	CONSTRAINT fk_mascotas_razas FOREIGN KEY(id_genero) 
+	REFERENCES tb_generos(id_genero),
+	id_raza INT,
+	CONSTRAINT fk_mascotas_genero FOREIGN KEY(id_raza) 
+	REFERENCES tb_razas(id_raza),
+	id_tipo_animal INT,
+	CONSTRAINT fk_mascotas_tipos_animales FOREIGN KEY(id_tipo_animal) 
+	REFERENCES tb_tipos_animales(id_tipo_animal)
 );
 
 CREATE TABLE tb_veterinarios(
@@ -95,7 +76,27 @@ CREATE TABLE tb_veterinarios(
 	CONSTRAINT fk_veterinarios_horarios_veterinarios 
 	FOREIGN KEY(id_horario_veterinario) 
 	REFERENCES tb_horarios_veterinarios(id_horario_veterinario)
-)
+);
+
+CREATE TABLE tb_citas(
+	id_cita INT AUTO_INCREMENT PRIMARY KEY,
+	causa_cita VARCHAR(255),
+	observaciones_cita VARCHAR(255),
+	medicasion_cita VARCHAR(255),
+	fecha_cita DATE,
+	id_mascota INT,
+	cantidad_citas INT,
+	precio_cita NUMERIC(5,2),
+	CONSTRAINT fk_cita_mascotas
+	FOREIGN KEY(id_mascota) 
+	REFERENCES tb_mascotas(id_mascota),
+	id_veterinario INT,
+	CONSTRAINT fk_cita_veterinarios
+	FOREIGN KEY(id_veterinario) 
+	REFERENCES tb_veterinarios(id_veterinario)
+);
+
+--- Inserts para la demostración
 INSERT INTO tb_generos(nombre_genero) VALUES ("Masculino");
 
 INSERT INTO tb_generos(nombre_genero) VALUES ("Femenino");
@@ -105,6 +106,8 @@ INSERT INTO tb_especialidades_veterianos(nombre_especialidad_veterinario) VALUES
 INSERT INTO tb_horarios_veterinarios(nombre_horario_veterinario) VALUES ("Matutino");
 
 INSERT INTO tb_tipos_animales(nombre_tipo_animal) VALUES ("Perro");
+
+INSERT INTO tb_razas(nombre_raza) VALUES ("Aguacatero");
 
 INSERT INTO tb_veterinarias(nombre_veterinaria, direccion_veterinaria) VALUES ("Huellitas", "San Luis");
 
@@ -168,6 +171,10 @@ VALUES ('Problemas digestivos', 'Prsenta malestar en estomago', '2024-01-29', 7,
 INSERT INTO tb_citas (causa_cita, observaciones_cita, fecha_cita, cantidad_citas, precio_cita) 
 VALUES ('Pelaje seco', 'Presenta pelaje reseco a falta de vitaminas', '2024-03-18', 5, 7.99);   
 
+
+
+
+
 -- EJEMPLO DE UNA SUBCONSULTA (De la tabla veterinarios queremos que seleccione el nombre de los veterinarios
 -- donde en la tabla veterinarias la direccion se encuentre en Mejicanos)
 
@@ -201,11 +208,6 @@ JOIN tb_veterinarias
 ON tb_veterinarios.id_veterinaria = tb_veterinarias.id_veterinaria
 
 
--- agregar campos a la tabla para realizar las funciones de SUM y AVG 
-ALTER TABLE tb_citas ADD COLUMN  precio_cita NUMERIC(3,2);
-ALTER TABLE tb_citas ADD COLUMN  cantidad_citas INT; 
-
-
 -- funcion SUM 
 
 SELECT SUM(cantidad_citas) FROM tb_citas; 
@@ -230,5 +232,5 @@ SELECT nombre_veterinario FROM tb_veterinarios LEFT JOIN
 tb_horarios_veterinarios ON tb_veterinarios.id_horario_veterinario = tb_horarios_veterinarios.id_horario_veterinario;
 
 -- Vistas con RIGHT JOIN
-	SELECT nombre_veterinario FROM tb_veterinarios RIGHT JOIN 
+SELECT nombre_veterinario FROM tb_veterinarios RIGHT JOIN 
 tb_horarios_veterinarios ON tb_veterinarios.id_horario_veterinario = tb_horarios_veterinarios.id_horario_veterinario;
